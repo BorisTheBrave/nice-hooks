33df2496c3dc6f06ab290948ecdbe9d0368491d9
Adam Newgas <adam2020@newgas.net>
2023-05-07 15:44:50 +0100

    Typo fix.
    Fixes #1

--- nice_hooks/__init__.py
+++ nice_hooks/__init__.py
@@ -1,13 +1,13 @@
 """The top-level package."""
 
 from .activationcache import ActivationCache, ActivationCacheLike
-from .nice_hooks import AnySlice, PathsLike, RemovableHandleCollection, ModulePath, expand_module_path, register_forward_hook, register_forward_pre_hook, register_full_backward_hook, register_full_backward_pre_hook, run, patch_method_to_module
+from .nice_hooks import AnySlice, ModulePathsLike, RemovableHandleCollection, ModulePath, expand_module_path, register_forward_hook, register_forward_pre_hook, register_full_backward_hook, register_full_backward_pre_hook, run, patch_method_to_module
 
 __all__ = [
     'ActivationCache',
     'ActivationCacheLike',
     'AnySlice', 
-    'PathsLike',
+    'ModulePathsLike',
     'to_paths',
     'RemovableHandleCollection',
     'ModulePath',
